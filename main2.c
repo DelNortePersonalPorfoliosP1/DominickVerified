3ea93d47bb86f1298e97d896bca79a44ab6ab9dd
Dominick <nativepanther802@gmail.com>
2019-09-13 09:20:59 -0700

    Create main2.c
    submititng the code

---
+++ main2.c
@@ -0,0 +1,37 @@
+//
+//  main2.c
+//  Lab
+//
+//  Created by Vanzant-Thomas, Dominick on 9/13/19.
+//  Copyright © 2019 Vanzant-Thomas, Dominick. All rights reserved.
+//
+
+
+main()
+{
+    int high = 100000;
+    int low = 0;
+    int guess = 1;
+    int myguess;
+    char reply;
+    
+    printf("The Game of 20 Questions\n");
+    printf("Think of a number between 0 and %d\n", high);
+    
+    while (high > low)
+    {
+        /* make a guess */
+        myguess = (high + low) / 2;
+        printf("Guess #%d: Is your number higher than %d? ",
+               guess++, myguess, high, low);
+        
+        /* get your response */
+        scanf(" %c",&reply);
+        
+        if (reply == 'y')
+            low = myguess = myguess + 1;
+        else
+            high = myguess;
+    }
+    printf("Your number is %d!\n", myguess);
+}
